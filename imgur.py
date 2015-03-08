bbe072e8147599f34ff35b48fc03796633036c4b
Jisu Kim <vbalien@live.jp>
2015-03-08 13:28:46 +0900

    Update and rename fufufuu.py to imgur.py

---
+++ imgur.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import http.client, os, sys
+from urllib.parse import urlparse
+
+def urlretrieve(url, path):
+    url = urlparse(url)
+    conn = http.client.HTTPConnection(url.hostname)
+    conn.request("GET", url.path)
+    r = conn.getresponse()
+    output = open(path,'wb')
+    output.write(r.read())
+    output.close()
+
+def downImgur(name, downDir = 'manga-down/'):
+    if not os.path.isdir(downDir) :
+        os.mkdir(downDir)
+    print('{0}::download start!'.format(name))
+    
+    conn = http.client.HTTPConnection(name+".imgur.com")
+    conn.request("GET", "")
+    r = conn.getresponse()
+    source = r.read()
+    source = source.decode("utf-8")
+    max = source.count('<div id="album-')
+    cnt = 0
+    while source.find('<div id="album-') != -1 :
+        cnt += 1
+        source = source[source.find('<div id="album-')+len('<div id="album-'):]
+        item_url = source[0:source.find('"')]
+        source = source[source.find('data-title="')+len('data-title="'):]
+        item_title = source[0:source.find('"')]
+        item_title = item_title.replace('/', '-').replace('\\', '-').replace(':', '').replace('*', '').replace('?', '').replace('"', '').replace('\'', '').replace('<', '').replace('>', '').replace('|', '')
+        
+        if not os.path.isdir(downDir+item_title) :
+            os.mkdir(downDir+item_title)
+        try:
+            print("[{0}/{1}][{3:3.0f}%]'{2}' Downloading...".format(cnt, max, 
+                item_title, 0), end="\r", flush=True)
+        except UnicodeEncodeError:
+            print("[{0}/{1}][{3:3.0f}%]'{2}' Downloading...".format(cnt, max, 
+                item_title.encode('utf-8'), 0), end="\r", flush=True)
+            
+        conn = http.client.HTTPConnection("imgur.com")
+        conn.request("GET", "/a/"+item_url)
+        item_res = conn.getresponse()
+        item_source = item_res.read()
+        item_source = item_source.decode("utf-8")
+        item_max = item_source.count('<a href="/download/')
+        i = 0
+        while item_source.find('<a href="/download/') != -1 :
+            item_source = item_source[item_source.find('<a href="/download/')+len('<a href="/download/'):]
+            imgname = item_source[0:item_source.find('"')]
+            urlretrieve("http://i.imgur.com/"+imgname+".jpg", downDir+item_title+"/"+str(i)+".jpg")
+            i += 1
+            print("[{0}/{1}][{3:3.0f}%]'{2}' Downloading...".format(cnt, max, item_title, 
+                i/item_max * 100), end="\r", flush=True)
+        print("")
+    print('{0}::done!'.format(name))
+
+if __name__ == '__main__':
+    for subname in sys.argv[1:]:
+        downImgur(subname)
